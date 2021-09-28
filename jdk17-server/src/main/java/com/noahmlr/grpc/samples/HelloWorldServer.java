9c86e53321d585852f711c9931f23b42bc0c7637
noahmlr <nmillercontact@gmail.com>
2021-09-28 12:50:16 -0500

    Initial Server commit

---
+++ jdk17-server/src/main/java/com/noahmlr/grpc/samples/HelloWorldServer.java
@@ -0,0 +1,62 @@
+package com.noahmlr.grpc.samples;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+public class HelloWorldServer {
+    private static final Logger logger = Logger.getLogger(HelloWorldServer.class.getName());
+
+    private final int port;
+
+    private Server server;
+
+    public HelloWorldServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException {
+        server = ServerBuilder.forPort(this.port)
+                .addService(new GreeterImpl())
+                .build()
+                .start();
+        logger.info("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+            System.err.println("*** shutting down gRPC server since JVM is shutting down");
+            try {
+                HelloWorldServer.this.stop();
+            } catch (InterruptedException e) {
+                e.printStackTrace(System.err);
+            }
+            System.err.println("*** server shut down");
+        }));
+    }
+
+    public void stop() throws InterruptedException {
+        if (server != null) {
+            server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+        }
+    }
+
+    public void blockUntilShutDown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+        @Override
+        public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+            var reply = HelloReply.newBuilder().setMessage("Hello " + request.getName()).build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+    }
+
+
+}
---
+++ jdk17-server/src/main/java/com/noahmlr/grpc/samples/Main.java
@@ -0,0 +1,16 @@
+package com.noahmlr.grpc.samples;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int port = 50052;
+
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        var server = new HelloWorldServer(port);
+        server.start();
+        server.blockUntilShutDown();
+    }
+}
