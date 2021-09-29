9087059f8203f2d91ee2f4686b9f195214e0a8c3
noahmlr <nmillercontact@gmail.com>
2021-09-29 10:36:55 -0500

    Implement fallback approach in client if GRPC is not stable

--- jdk17-spring-client/src/main/java/com/noahmlr/grpc/samples/NewtonMathClient.java
+++ jdk17-spring-client/src/main/java/com/noahmlr/grpc/samples/NewtonMathClient.java
@@ -1,8 +1,17 @@
 package com.noahmlr.grpc.samples;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.Http;
 import io.grpc.Channel;
 import org.springframework.stereotype.Service;
 
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,8 +20,13 @@ public class NewtonMathClient {
     Logger logger = Logger.getLogger(NewtonMathClient.class.getName());
 
     private final NewtonMathGrpc.NewtonMathFutureStub futureStub;
+    private final HttpClient httpClient;
 
     public NewtonMathClient(Channel channel) {
+        httpClient = HttpClient
+                .newBuilder()
+                .proxy(ProxySelector.getDefault())
+                .build();
         futureStub = NewtonMathGrpc.newFutureStub(channel);
     }
 
@@ -21,6 +35,24 @@ public class NewtonMathClient {
             return futureStub.performOperation(request).get();
         } catch (Exception ex) {
             logger.log(Level.SEVERE, "Failed to perform calculation", ex);
+            return fallback(request);
+        }
+    }
+
+    // USED FOR TESTING PURPOSES IF GRPC IS DOWN / NOT WORKING
+    public MathCalculationReply fallback(MathCalculationRequest request) {
+        try {
+            var httpRequest = HttpRequest.newBuilder()
+                    .uri(new URI(String.join("/", "https://newton.vercel.app/api/v2",
+                            request.getOperation(),
+                            URLEncoder.encode(request.getExpression(), StandardCharsets.UTF_8)))
+                    )
+                    .GET()
+                    .build();
+            var response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            return new ObjectMapper().readValue(response.body(), MathCalculationResult.class).reply();
+        } catch (Exception ex) {
+            logger.log(Level.SEVERE, "Failed to fall back", ex);
             throw new RuntimeException(ex);
         }
     }
--- jdk17-spring-client/src/main/java/com/noahmlr/grpc/samples/NewtonMathController.java
+++ jdk17-spring-client/src/main/java/com/noahmlr/grpc/samples/NewtonMathController.java
@@ -1,6 +1,8 @@
 package com.noahmlr.grpc.samples;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NewtonMathController {
@@ -13,8 +15,10 @@ public class NewtonMathController {
 
     @PostMapping("/calculate")
     private MathCalculationResult performCalculation(@RequestBody MathCalculation calculation) {
-        var reply = newtonMathClient.performCalculation(calculation.request());
+        var request = calculation.request();
+        var reply = newtonMathClient.performCalculation(request);
         return new MathCalculationResult(reply.getOperation(), reply.getExpression(), reply.getResult());
+
     }
 
 }
